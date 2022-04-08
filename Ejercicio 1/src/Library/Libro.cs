146e20a9dee67cf25bc8f8a6f4a3b58cdefb1edf
amandaseara <amanda.seara@correo.ucu.edu.uy>
2022-04-07 22:26:55 -0300

    Critica del ejercicio 1

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -18,6 +18,10 @@ namespace SRP
             this.Codigo = codigo;
         }
 
+        
+        //Segun SRP, cada clase debe tener responsabilidad sobre una parte de la funcionalidad 
+        //y la responsabilidad debe estar completamente encapsulada por la clase.
+        //Esto no se cumple porque la clase libro tiene dos metodos que cumplen dos responsabilidades distintas.
         public void AlmacenarLibro(String sector, String estante)
         {
             this.SectorBiblioteca = sector;
